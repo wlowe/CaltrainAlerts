74fd99f3dc43ba8aa6273a98429779d1498cf2d3
Walter Lowe <jebcor@gmail.com>
2011-10-23 14:53:17 -0700

    action bars work in progress.

--- CaltrainUpdatesApp/src/com/lowetech/caltrainupdates/android/Main.java
+++ CaltrainUpdatesApp/src/com/lowetech/caltrainupdates/android/Main.java
@@ -2,34 +2,28 @@ package com.lowetech.caltrainupdates.android;
 
 import java.util.HashMap;
 
-import android.app.ListActivity;
-import android.content.Intent;
-import android.database.Cursor;
 import android.os.Bundle;
 import android.os.Handler;
+import android.support.v4.app.FragmentActivity;
 import android.view.View;
-import android.view.View.OnClickListener;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
-import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
-import android.widget.SimpleCursorAdapter;
 
 import com.google.android.c2dm.C2DMessaging;
-import com.lowetech.caltrainupdates.android.Constants.TrainUpdates;
 
-public class Main extends ListActivity implements ServerEventListener 
+public class Main extends FragmentActivity implements ServerEventListener 
 {		
 	
-	/**
-     * The columns we are interested in from the database
-     */
-    private static final String[] PROJECTION = new String[] {
-            TrainUpdates._ID, // 0
-            TrainUpdates.DATE, // 1
-            TrainUpdates.TEXT
-    };
+//	/**
+//     * The columns we are interested in from the database
+//     */
+//    private static final String[] PROJECTION = new String[] {
+//            TrainUpdates._ID, // 0
+//            TrainUpdates.DATE, // 1
+//            TrainUpdates.TEXT
+//    };
     
     private static final String USER_REFRESH_STATE = "userRefreshState";
     private boolean manualRefreshInProgress = false;
@@ -58,49 +52,49 @@ public class Main extends ListActivity implements ServerEventListener
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-        final ImageButton buttonRefresh = (ImageButton) findViewById(R.id.buttonRefresh);
+//        final ImageButton buttonRefresh = (ImageButton) findViewById(R.id.buttonRefresh);
+//        
+//        buttonRefresh.setOnClickListener(new OnClickListener()
+//		{
+//			
+//			public void onClick(View v)
+//			{
+//				manualRefreshInProgress = true;
+//				setSpinnerState(true);
+//				ServiceHelper.fetchUpdates(getApplicationContext());												
+//			}
+//		});
+//        
+//        Button buttonRegister = (Button)findViewById(R.id.buttonRegister);
+//        
+//        buttonRegister.setOnClickListener(new OnClickListener()
+//		{
+//			
+//			public void onClick(View v)
+//			{
+//				
+//				//C2DMessaging.register(getApplicationContext(), Constants.C2DM_SENDER);
+//				NotificationsHandler.showNotification("ticker", "title", "message", getApplicationContext(), 3000);
+//			}
+//		});
         
-        buttonRefresh.setOnClickListener(new OnClickListener()
-		{
-			
-			public void onClick(View v)
-			{
-				manualRefreshInProgress = true;
-				setSpinnerState(true);
-				ServiceHelper.fetchUpdates(getApplicationContext());												
-			}
-		});
         
-        Button buttonRegister = (Button)findViewById(R.id.buttonRegister);
-        
-        buttonRegister.setOnClickListener(new OnClickListener()
-		{
-			
-			public void onClick(View v)
-			{
-				
-				//C2DMessaging.register(getApplicationContext(), Constants.C2DM_SENDER);
-				NotificationsHandler.showNotification("ticker", "title", "message", getApplicationContext(), 3000);
-			}
-		});
-        
-        
-        Intent intent = getIntent();
-        intent.setData(TrainUpdates.CONTENT_URI);
-        
-        // Perform a managed query. The Activity will handle closing and requerying the cursor
-        // when needed.
-        Cursor cursor = managedQuery(intent.getData(), PROJECTION, null, null,
-                TrainUpdates.DEFAULT_SORT_ORDER);
-
-        // Used to map notes entries from the database to views
-        SimpleCursorAdapter adapter = new SimpleCursorAdapter(
-        		this, 
-        		R.layout.two_line_list_item, 
-        		cursor,
-                new String[] { TrainUpdates.DATE, TrainUpdates.TEXT }, 
-                new int[] { R.id.date, R.id.updateText });
-        setListAdapter(adapter);	
+//        Intent intent = getIntent();
+//        intent.setData(TrainUpdates.CONTENT_URI);
+//        
+//        // Perform a managed query. The Activity will handle closing and requerying the cursor
+//        // when needed.
+//        Cursor cursor = managedQuery(intent.getData(), PROJECTION, null, null,
+//                TrainUpdates.DEFAULT_SORT_ORDER);
+//
+//        // Used to map notes entries from the database to views
+//        SimpleCursorAdapter adapter = new SimpleCursorAdapter(
+//        		this, 
+//        		R.layout.two_line_list_item, 
+//        		cursor,
+//                new String[] { TrainUpdates.DATE, TrainUpdates.TEXT }, 
+//                new int[] { R.id.date, R.id.updateText });
+//        setListAdapter(adapter);	
         
         if (C2DMessaging.getRegistrationId(getApplicationContext()).length() == 0)
         {
